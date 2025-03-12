6c725547fe8b86d742f85b6ab9f2f44f7a89841f
nyelgireddy <nyelgireddy@gmail.com>
2025-03-12 16:18:42 -0700

    “message”

--- a-frame/clock.js
+++ a-frame/clock.js
@@ -1,37 +1,42 @@
 AFRAME.registerComponent("clock", {
-  schema: {
-  },
+  schema: {},
 
   init: function () {
-    //Create a new element, set 
+    // Create text element for the clock display
     this.titleEl = document.createElement("a-text");
     this.titleEl.setAttribute("position", { x: 1, y: 0, z: 0 });
     this.titleEl.setAttribute("color", "#00ff00");
     this.titleEl.setAttribute("font", "sourcecodepro");
-    this.titleEl.setAttribute("value", "Hello, world!");
-
     this.el.appendChild(this.titleEl);
 
+    // List of time zones to cycle through
+    this.timeZones = ["Local", "London", "New York", "Tokyo"];
+    this.currentIndex = 0;
 
-    //TODO: Use helper functions in timezone.js to display the current time and support converting time zones
-    //For implementing interactivity, you may find .addEventListener() useful
-    //https://aframe.io/docs/1.2.0/introduction/interactions-and-controllers.html#events
+    // Update time initially
+    this.updateTime();
+
+    // Add click interaction to cycle through time zones
+    this.el.addEventListener("click", () => {
+      this.currentIndex = (this.currentIndex + 1) % this.timeZones.length;
+      this.updateTime();
+    });
+
+    // Update time every second
+    this.interval = setInterval(() => this.updateTime(), 1000);
   },
 
-  tick: function () {
+  updateTime: function () {
+    let now = spacetime.now();
+    let timeZone = this.timeZones[this.currentIndex];
+    let convertedTime = timeZone === "Local" ? now : convertTimeZone(now, timeZone);
+    let displayString = `Time in ${timeZone}: ${displayTime(convertedTime)}`;
+
+    // Update text display
+    this.titleEl.setAttribute("value", displayString);
   },
+
+  remove: function () {
+    clearInterval(this.interval); // Stop interval when the component is removed
+  }
 });
-
-var now = spacetime.now();
-
-console.log("It is currently " + displayTime(now) + " locally");
-console.log(
-  "It is currently " +
-    displayTime(convertTimeZone(now, "London")) +
-    " in London"
-);
-console.log(
-  "2:42pm in local time is " +
-    displayTime(convertTimeZone(getTime("2:42pm"), "Hawaii")) +
-    " in Hawaii"
-);
