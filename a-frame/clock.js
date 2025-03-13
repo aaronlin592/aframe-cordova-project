c2f5d944b29692d9b798dca91aa99cc9a7193584
Aaron Lin <shihsyul@uci.edu>
2025-03-12 17:14:24 -0700

    added big blue circle background and implementation for time zone cycle and tooltips

--- a-frame/clock.js
+++ a-frame/clock.js
@@ -4,10 +4,28 @@ AFRAME.registerComponent("clock", {
   init: function () {
     // Create text element for the clock display
     this.titleEl = document.createElement("a-text");
-    this.titleEl.setAttribute("position", { x: 1, y: 0, z: 0 });
+    this.titleEl.setAttribute("position", { x: 2.4, y: -0.5, z: 0 });
     this.titleEl.setAttribute("color", "#00ff00");
     this.titleEl.setAttribute("font", "sourcecodepro");
+    this.titleEl.setAttribute("align", "center");
     this.el.appendChild(this.titleEl);
+
+    // tooltip/hint
+    this.tooltip = document.createElement("a-text");
+    this.tooltip.setAttribute("position", { x: 2.4, y: -1, z: 0 });
+    this.tooltip.setAttribute("color", "#ff0000");
+    this.tooltip.setAttribute("font", "sourcecodepro");
+    this.tooltip.setAttribute("value", "Press anywhere in the blue to change time zones!");
+    this.tooltip.setAttribute("scale", "0.5 0.5 0.5");
+    this.tooltip.setAttribute("align", "center");
+    this.el.appendChild(this.tooltip);
+
+    // circle background
+    this.changeTimeZoneButton = document.createElement("a-circle");
+    this.changeTimeZoneButton.setAttribute("position", { x: 2.4, y: -0.5, z: 0 });
+    this.changeTimeZoneButton.setAttribute("color", "#0000FF");
+    this.changeTimeZoneButton.setAttribute("radius", "1.6");
+    this.el.appendChild(this.changeTimeZoneButton);
 
     // List of time zones to cycle through
     this.timeZones = ["Local", "London", "New York", "Tokyo"];
